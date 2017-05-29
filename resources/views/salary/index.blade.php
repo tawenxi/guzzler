3b486d32a37d980d8c909d1524a3cc799da395aa
tawenxi <tawenxi@qq.com>
2017-05-29 07:58:26 +0000

    memberinsert4

--- resources/views/salary/index.blade.php
+++ resources/views/salary/index.blade.php
@@ -11,38 +11,94 @@
 
 		<thead>
 			<tr class='success'>
-				<th>ID</th><th>姓名</th><th>基本工资</th><th>津补贴</th>
+				<th>ID</th><th>姓名</th>
+
+
+				@if ($res->sum('tuixiu_gz'))
+				
+				<th>	{{$res->tuixiu_gz}}</th>
+				@endif
+
+				
+				<th>基本工资</th><th>津补贴</th>
+				<th>车补</th>
+				<th>补发工资</th>
+				<th>单位公积金</th><th>单位社保</th>
+
+				<th>应发合计</th>
 			</tr>
 		</thead>
 		<tbody class='alert-info'>
 
-				@foreach ($res as $re)
+				@foreach ($res as $res)
 			<tr class={{ empty($re->id)?'alert-danger':""}}>
 				<td>
 				
-					<a href="{{ $re->id }}/show">{{$re->id}} 
+					<a href="{{ $res->id }}/show">{{$res->id}} 
 
 					</a>
 				
 				</td>
 				<td>
 				
-					{{$re->name}}
+					{{$res->name}}
+				
+				</td>
+								<td>
+								@if ($res->tuixiu_gz)
+				
+					{{$res->tuixiu_gz}}
+				@endif
+					
 				
 				</td>
 				<td>
 				
-					{{$re->jiben_gz}}
+					{{$res->jiben_gz}}
 				
 				</td>
 				<td>
 				
-					{{$re->jinbutie}}
+					{{$res->jinbutie}}
+				
+				</td>
+								<td>
+				
+					{{$res->gongche_bz}}
+				
+				</td>
+							<td>
+				
+					{{$res->bufa_gz}}
+				
+				</td>	<td>
+				
+					{{$res->gjj_dw}}
+				
+				</td>
+								<td>
+				
+					{{$res->sb_dw}}
+				
+				</td>
+
+												<td>
+				
+					{{$res->yishu_bz+
+						$res->tuixiu_gz+
+						$res->jiben_gz+
+						$res->jinbutie+
+						$res->sb_dw+
+						$res->gjj_dw+
+						$res->bufa_gz+
+						$res->gongche_bz}}
+
+
 				
 				</td>
 				<td class='btn btn-link'>
 				
-					<a href="edit/{{ $re->ok }}" > 编辑</a>
+					<a href="edit/{{ $res->ok }}" > 编辑</a>
 				
 				</td>
 			</tr>	
